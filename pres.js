5639ac8f1df3788b1a6dd65b8eb8fa5157d2a0da
jedediahriley <46664082+jedediahriley@users.noreply.github.com>
2021-02-28 12:46:01 -0500

    created teh edit and index. ejs files

--- pres.js
+++ pres.js
@@ -2,7 +2,29 @@ const express = require('express');
 const APP = express();
 const PORT = 3000;
 
+// Dependencies
+const mongoose = require('mongoose');
+const Add = require('./models/addequip.js');
 
+// Global configuration
+const mongoURI = 'mongodb://localhost:27017/equipmentpreservation'
+const db = mongoose.connection;
+
+// Connecting to Mongo
+mongoose.connect(mongoURI, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+},  () => {
+    console.log('connected to Mongo');
+});
+
+// Routes
+
+APP.get('/equipment', (req, res) => {
+    res.send('index')
+
+});
 
 APP.listen(PORT, () => {
     console.log('hello')
